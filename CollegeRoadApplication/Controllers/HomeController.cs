7100f840cc01b71d1265dbe2cad8fc70a5c75d36
Joe Heath <heath_j09@outlook.com>
2021-01-03 20:07:57 +0000

    Home page, swimming meet and event design

--- CollegeRoadApplication/Controllers/HomeController.cs
+++ CollegeRoadApplication/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace CollegeRoadApplication.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public ActionResult Index()
